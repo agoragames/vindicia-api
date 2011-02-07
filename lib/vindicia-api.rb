52a7d5c365cc4617db8ea93b51345072cb4bea76
Tom Quackenbush <tquackenbush@agoragames.com>
2011-02-07 15:51:05 -0500

    first pass at extending savon_model to include vindicia specific parameters. needs tests.

--- lib/vindicia-api.rb
+++ lib/vindicia-api.rb
@@ -1,8 +1,11 @@
+require 'vindicia/model'
+
 module Vindicia
   
   API_CLASSES = {
     "3_5" => {
         :account => [],
+        :product => [:fetch_all],
         :entitlement => [:fetch_by_entitlement_id_and_account],
         :transaction => []
       }
@@ -10,15 +13,42 @@ module Vindicia
 
   class Entitlement
     include Savon::Model
+    include Vindicia::Model
 
     client do
       http.headers["Pragma"] = "no-cache"
+      http.auth.ssl.verify_mode = :none
     end
 
+    vindicia_class 'Entitlement'
+    api_version '3.5'
+    login 'mlg_soap'
+    password 'FNWV3iFIpw76gt3qpkfVCe4ZEoIx2fek'
+    
     endpoint "https://soap.prodtest.sj.vindicia.com/soap.pl"
     namespace "http://soap.vindicia.com/v3_5/Entitlement"
 
     actions *API_CLASSES['3_5'][:entitlement]
   end
 
+  class Product
+    include Savon::Model
+    include Vindicia::Model
+
+    client do
+      http.headers["Pragma"] = "no-cache"
+      http.auth.ssl.verify_mode = :none
+    end
+
+    vindicia_class 'Product'
+    api_version '3.5'
+    login 'mlg_soap'
+    password 'FNWV3iFIpw76gt3qpkfVCe4ZEoIx2fek'
+
+    endpoint "https://soap.prodtest.sj.vindicia.com/soap.pl"
+    namespace "http://soap.vindicia.com/v3_5/Product"
+
+    actions *API_CLASSES['3_5'][:product]
+  end
 end
+
---
+++ lib/vindicia/model.rb
@@ -0,0 +1,97 @@
+require "savon"
+
+module Vindicia
+
+  # = Vindicia::Model
+  #
+  # Model for SOAP service oriented applications.
+  module Model
+
+    VERSION = "0.0.0"
+
+    module ClassMethods
+
+      def self.extend_object(base)
+        super
+        base.init_vindicia_model
+      end
+
+      def init_vindicia_model
+        class_action_module
+        instance_action_module
+      end
+
+      # Accepts one or more SOAP actions and generates both class and instance methods named
+      # after the given actions. Each generated method accepts an optional SOAP body Hash and
+      # a block to be passed to <tt>Savon::Client#request</tt> and executes a SOAP request.
+      def actions(*actions)
+        super
+        actions.each do |action|
+          alias_class_action action
+          alias_instance_action action
+        end
+      end
+
+      def vindicia_class(vindicia_class_name)
+        @vindicia_class_name = vindicia_class_name
+      end
+      
+      def api_version(version)
+        @api_version = version
+      end
+      
+      def login(login)
+        @login = login
+      end
+      
+      def password(password)
+        @password = password
+      end
+      
+    private
+
+      def vindicia_auth_credentials
+        {:login => @login, :password => @password, :version => @api_version}
+      end
+      
+      def vindicia_target_namespace
+        "http://soap.vindicia.com/v3_5/#{@vindicia_class_name}"
+      end
+
+      def vindicia_soap_action(action)
+        %{"http://soap.vindicia.com/v3_5/#{@vindicia_class_name}##{action.to_s.camelcase.gsub(/^./) {|s| s.downcase }}"}
+      end
+      
+      def alias_class_action(action)
+        class_action_module.module_eval <<-CODE
+          def #{action.to_s.snakecase}_with_vindicia(body = nil, &block)
+            client.request :tns, #{action.inspect}, :body => body do
+              soap.namespaces["xmlns:tns"] = vindicia_target_namespace
+              http.headers["SOAPAction"] = vindicia_soap_action('#{action}')
+              soap.body = {
+                :auth => vindicia_auth_credentials
+              }
+              block.call(soap, wsdl, http, wsse) if block
+            end
+          end
+          alias_method_chain :#{action.to_s.snakecase}, :vindicia
+        CODE
+      end
+
+      def alias_instance_action(action)
+        instance_action_module.module_eval <<-CODE
+          def #{action.to_s.snakecase}_with_vindicia(body = nil, &block)
+            self.class.#{action.to_s.snakecase} body, &block
+          end
+          alias_method_chain :#{action.to_s.snakecase}, :vindicia
+        CODE
+      end
+    end
+    
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+  end
+end
+
