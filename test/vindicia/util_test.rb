e29395d77a0968acfbcbc57c4ba5387faf7a9671
Andrew Nordman <cadwallion@gmail.com>
2012-09-12 15:43:32 -0500

    Replaces Vindicia::Util with ActiveSupport
    Uses camelize and underscore methods

--- test/vindicia/util_test.rb
+++
@@ -1,8 +0,0 @@
-require 'helper'
-require 'vindicia/util'
-
-class Vindicia::UtilTest < Test::Unit::TestCase
-  def test_camelize_should_produce_camalcase_name_from_lowercase_underscore_name
-    assert_equal 'FooBarBaz', Vindicia::Util.camelize('foo_bar_baz')
-  end
-end
