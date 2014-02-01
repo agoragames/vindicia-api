bb6c2326b7ea9af72474392f19d54f12fca8a7a7
James Gorlick <l2paegun@hotmail.com>
2014-01-31 18:14:20 -0800

    fixed savon helper to properly drill in on Response.to_class, yielding the payload

--- lib/vindicia/savon.rb
+++ lib/vindicia/savon.rb
@@ -1,10 +1,13 @@
 module Savon
   module SOAP
     class Response
-      def self.to_class()
+      def to_class()
         return nil if !success?
         klass = to_hash
-        klass[klass.keys[0]][1]
+        # drill down to envelope
+        klass = klass[klass.keys[0]]
+        # drill down to payload, [0] is return, [1] is payload, [2] is xmlns
+        klass[klass.keys[1]]
       end
     end
   end
